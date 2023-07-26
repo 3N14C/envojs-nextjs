9745f39ddce2ae83932ef55401cd31649a6edd00
3N14C <3n14c@vk.com>
2023-07-26 16:32:58 +0800

    fixed animations

--- app/components/services/screens/adaptive/Mobile.jsx
+++ app/components/services/screens/adaptive/Mobile.jsx
@@ -5,8 +5,19 @@ import Image from "next/image";
 import { useRef } from "react";
 
 const ToolItem = ({ toolIcon, toolTitle, toolDescr }) => {
+  const ref = useRef();
+  const isInView = useInView(ref, { once: true });
+
   return (
-    <div className={`${styles.tools}`}>
+    <div
+    ref={ref}
+      style={{
+        transform: isInView ? "none" : "translateX(-200px)",
+        opacity: isInView ? 1 : 0,
+        transition: "all .9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+      }}
+      className={`${styles.tools}`}
+    >
       <div className={`${styles.tools_icon_container}`}>
         <Image
           className={`${styles.tools_icon}`}
@@ -29,16 +40,15 @@ const Mobile = () => {
 
   return (
     <div ref={ref} className="">
-      <div
-        style={{
-          transform: isInView ? "none" : "translateX(-200px)",
-          opacity: isInView ? 1 : 0,
-          transition: "all .9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-        }}
-        id="services"
-        className={`${styles.mobile}`}
-      >
-        <div className={`${styles.mobile_inner}`}>
+      <div id="services" className={`${styles.mobile}`}>
+        <div
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all .9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+          className={`${styles.mobile_inner}`}
+        >
           <div className={`${styles.mobile_icon_pause}`}>
             <Image
               className={``}
