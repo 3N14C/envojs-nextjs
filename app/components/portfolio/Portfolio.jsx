975bbb745ad3cb42a4099a59abac85bc4f8204e3
3N14C <3n14c@vk.com>
2023-07-27 19:08:20 +0800

    minor fixes

--- app/components/portfolio/Portfolio.jsx
+++ app/components/portfolio/Portfolio.jsx
@@ -1,29 +1,19 @@
 "use client";
-<<<<<<< HEAD
 
-import MediaQuery from 'react-responsive'
-import { Mobile } from './screens/adaptive/Mobile'
-import { Normal } from './screens/normal/Normal'
+import MediaQuery from "react-responsive";
+import { Mobile } from "./screens/adaptive/Mobile";
+import { Normal } from "./screens/normal/Normal";
 
 export default function Portfolio() {
-=======
-
-import MediaQuery from 'react-responsive'
-import { Mobile } from './screens/adaptive/Mobile'
-import { Normal } from './screens/normal/Normal'
-
-const Portfolio = () => {
->>>>>>> 906c58691dcebcfa0b202cac6b8761ec944e2182
-
   return (
-		<>
-			<MediaQuery minWidth={576}>
-				<Normal />
-			</MediaQuery>
+    <>
+      <MediaQuery minWidth={576}>
+        <Normal />
+      </MediaQuery>
 
       <MediaQuery maxWidth={576}>
         <Mobile />
       </MediaQuery>
-		</>
-	)
-};
+    </>
+  );
+}
